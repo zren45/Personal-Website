45626b8c8302786a2c4e70e19869929dd44654ba
zren45 <zhixiangren@gatech.edu>
2021-12-29 13:46:48 -0600

    change fonts

---
+++ components/fonts.js
@@ -0,0 +1,10 @@
+import { Global } from "@emotion/react";
+
+const Fonts = () => (
+  <Global
+    styles={`
+    @import url('https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap');
+      `}
+  />
+);
+export default Fonts;
--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout/main";
 import { AnimatePresence } from "framer-motion";
 import theme from "../lib/theme";
+import Fonts from "../components/fonts";
 
 function App({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout router={router}>
-        <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
+      <Fonts>
+        <Layout router={router}>
+          <AnimatePresence exitBeforeEnter initial={true}>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        </Layout>
+      </Fonts>
     </ChakraProvider>
   );
 }
