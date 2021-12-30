94eae3c6b9f09c70595e286c82c346b1e23a9c92
zren45 <zhixiangren@gatech.edu>
2021-12-30 15:57:04 -0600

    fix(fonts): use styled jsx instead
    to avoid initial loading issue on iOS Safari

--- components/fonts.js
+++ components/fonts.js
@@ -1,10 +1,8 @@
-import { Global } from "@emotion/react";
-
 const Fonts = () => (
-  <Global
-    styles={`
-    @import url('https://fonts.googleapis.com/css2?family=Inter&family=Merriweather&display=swap');
-      `}
-  />
+  <style jsx global>
+    {`
+      @import url("https://fonts.googleapis.com/css2?family=Inter&family=Merriweather&display=swap");
+    `}
+  </style>
 );
 export default Fonts;
