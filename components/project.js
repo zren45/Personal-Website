b83194eb696c095d7e39ae84ecc64963baea7678
zren45 <zhixiangren@gatech.edu>
2022-01-10 15:43:01 -0600

    Update project.js
    lighter shadow

--- components/project.js
+++ components/project.js
@@ -8,7 +8,7 @@ function Project({ children }) {
         y: -10,
 
         boxShadow:
-          "7px 7px 10px rgb(161, 117, 235,0.75),15px 15px 15px rgb(160, 233, 255), -7px -7px 10px rgb(237, 202, 133,0.75),-15px -15px 15px rgb(224, 137,188,1)",
+          " 7px 7px 10px rgb(161, 117, 235,0.1),15px 15px 15px rgb(160, 233, 255,0.3), -7px -7px 10px rgb(237, 202, 133,0.1),-15px -15px 15px rgb(224, 137,188,0.3)",
       }}
       transition={{ ease: "easeOut", duration: 0.7 }}
       layout
@@ -21,12 +21,11 @@ function Project({ children }) {
       <Container
         bg={useColorModeValue("white", "#191919")}
         maxW="100%"
-        height="xl"
         borderRadius={30}
+        p={(50, 100)}
+        textAlign={["left", "right"]}
       >
-        <Center height="100%" width="100%">
-          {children}
-        </Center>
+        {children}
       </Container>
     </motion.div>
   );
