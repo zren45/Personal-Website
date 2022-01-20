21400e6d13ffddee41ec12e136bf7cea9eff59c4
zren45 <zhixiangren@gatech.edu>
2022-01-19 23:59:40 -0600

    Update post.js

--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,32 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../components/layout/article";
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  Button,
+  useColorModeValue,
+  Box,
+  Container,
+} from "@chakra-ui/react";
 
 export default function FirstPost() {
   return (
     <Layout title="Post">
-      <div>
-        <Heading> In Progress</Heading>
-        <Text>Weeely Task Report and Study Plan</Text>
-      </div>
+      <Container>
+        <Heading my={6}> In Progress</Heading>
+        <Text my={6}>Weeely Task Report and Study Plan</Text>
+        <Box my={6} align={"center"}>
+          <Link href="/">
+            <Button
+              color={useColorModeValue("white", "gray.800")}
+              bg={useColorModeValue("purple.900", "orange.100")}
+            >
+              Return to home
+            </Button>
+          </Link>
+        </Box>
+      </Container>
     </Layout>
   );
 }
