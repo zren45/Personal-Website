ff60f5f6134f3830296bb26c7974aed471e4208e
zren45 <zhixiangren@gatech.edu>
2022-01-19 23:12:38 -0600

    Update first-post.js
    placeholder

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../components/layout/article";
+import { Heading, Text } from "@chakra-ui/react";
 
 export default function FirstPost() {
   return (
     <Layout title="Post">
-      <div>hello world</div>;
+      <div>
+        <Heading> In Progress</Heading>
+        <Text>Weeely Task Report and Study Plan</Text>
+      </div>
     </Layout>
   );
 }
