45c17eee72bcf0bdd1ba026a7aed315daecd0580
zren45 <zhixiangren@gatech.edu>
2022-01-21 20:18:23 -0600

    Update pronunciation.js

--- pages/pronunciation.js
+++ pages/pronunciation.js
@@ -1,11 +1,25 @@
 import Layout from "../components/layout/article";
-import { Container, Heading, Text } from "@chakra-ui/react";
+import { Container, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 
 export default function Pronunciation() {
   return (
     <Layout title="pronunciation">
       <Container>
-        <Heading> How do you pronunce your name ? </Heading>
+        <Heading
+          as="h3"
+          mb={4}
+          bgGradient={useColorModeValue(
+            "linear(to-l,#18c9ff 30%,#8627f1 50%,#f91f63 70%,#ffb420 94%)",
+            "linear(to-l, #a0e9ff 30%,#a162e8 50%,#f093b0 70%,#edca85 94%)"
+          )}
+          bgClip="text"
+        >
+          {" "}
+          How do you pronounce your name ?{" "}
+        </Heading>
+        <Text fontSize="lg">
+          This is probably the quesion i get asked the most.{" "}
+        </Text>
       </Container>
     </Layout>
   );
