7d14273519783ff681570b9b68de9a891dfd0dc5
zren45 <zhixiangren@gatech.edu>
2022-02-22 17:54:25 -0600

    display md file

---
+++ pages/posts/[id].js
@@ -0,0 +1,33 @@
+import Layout from "../../components/layout/article";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout title={postData.id}>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
