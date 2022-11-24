3480b5b2c200bd6b4df4281a0f4402e1d79d0887
Zhixiang Ren <zhixiangren@gatech.edu>
2022-11-24 14:18:41 -0600

    stepn calculator X

--- pages/calculator.js
+++ pages/calculator.js
@@ -1,9 +1,11 @@
 import Layout from "../components/layout/article";
 import MintCalculator from "../components/mint-calculator";
+import { Heading } from "@chakra-ui/react";
 
 export default function MintProfit() {
   return (
     <Layout title={"Mint Calculator"}>
+      <Heading>this tool is deprecated </Heading>
       <MintCalculator />
     </Layout>
   );
